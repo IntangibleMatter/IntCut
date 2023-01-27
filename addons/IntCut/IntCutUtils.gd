31d8dec71a80ca53c52c1c5ff8e9815b37d848ff
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2023-01-27 15:26:33 -0800

    small fix

--- addons/IntCut/IntCutUtils.gd
+++ addons/IntCut/IntCutUtils.gd
@@ -3,4 +3,10 @@ class_name IntCutUtils
 
 func world_to_screen(world_coordinates: Vector2) -> Vector2:
 	var cam := get_viewport().get_camera_2d()
-	return world_coordinates - cam.position
+	return world_coordinates - cam.global_position
+
+
+func pos_in_cam(world_coordinates: Vector2) -> bool:
+	var cam := get_viewport().get_camera_2d()
+	
+	return false
