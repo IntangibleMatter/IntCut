18bff68cf16ce5eb5b47b71770e9c177944a9402
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2023-06-06 18:55:26 -0700

    it's alivvve!

--- addons/IntCut/display/dialoguebubble/dialogue_bubble.gd
+++ addons/IntCut/display/dialoguebubble/dialogue_bubble.gd
@@ -15,28 +15,36 @@ var bubble_colour := Color.BLACK
 
 var corner_points_template : Array[PackedVector2Array] = [
 	[ # top left
-		Vector2(-1, 0),
+		Vector2(-1, -0.1),
 		Vector2(-0.9, -0.4),
+		Vector2(-0.8, -0.6),
+		Vector2(-0.6, -0.8),
 		Vector2(-0.4, -0.9),
-		Vector2(0, -1)
+		Vector2(-0.1, -1),
 	],
 	[ # top right
-		Vector2(0, -1),
+		Vector2(0.1, -1),
 		Vector2(0.4, -0.9),
+		Vector2(0.6, -0.8),
+		Vector2(0.8, -0.6),
 		Vector2(0.9, -0.4),
-		Vector2(1, 0)
+		Vector2(1, -0.1),
 	],
 	[ # bottom right
-		Vector2(1, 0),
+		Vector2(1, 0.1),
 		Vector2(0.9, 0.4),
+		Vector2(0.8, 0.6),
+		Vector2(0.6, 0.8),
 		Vector2(0.4, 0.9),
-		Vector2(0, 1)
+		Vector2(0.1, 1),
 	],
 	[ # bottom left
-		Vector2(0, 1),
+		Vector2(-0.1, 1),
 		Vector2(-0.4, 0.9),
+		Vector2(-0.6, 0.8),
+		Vector2(-0.8, 0.6),
 		Vector2(-0.9, 0.4),
-		Vector2(-1, 0)
+		Vector2(-1, 0.1),
 	],
 ]
 var corner_scale : float = 1
@@ -45,6 +53,7 @@ var corner_scale : float = 1
 @export var speaker: Node2D
 @export var pos_flag : POS_FLAGS
 
+@export var bubble_oscilate_speed : float = 500
 @export var bubble_corner_size := 1
 @export var bubble_point_move_scale := 2
 @export var bubble_padding := 1
@@ -55,12 +64,12 @@ var corner_scale : float = 1
 func _ready() -> void:
 	bubble_rect = Rect2(calculate_bubble_location(), Vector2.ZERO)
 	calculate_bubble_points(bubble_rect)
-	rich_text_label.text = """[center][shake]What the fuck is wrong with you[/shake]"""
+	rich_text_label.text = """[center][shake rate=20 level=20]What the fuck is wrong with you[/shake]"""
 	waittt()
 
 func _process(delta: float) -> void:
-	prints("template", corner_points_template)	
-	update_bubble_points(delta)
+#	prints("template", corner_points_template)
+	update_bubble_points()
 	update_speech_line()
 	queue_redraw()
 
@@ -84,8 +93,8 @@ func scale_dialogue_box() -> void:
 	
 	var tween := create_tween().set_parallel(true)
 	var new_rect : Rect2 = Rect2(Vector2(256, 256), rich_text_label.size)
-	tween.tween_property(self, "bubble_rect", new_rect, 0.2)
-	tween.tween_method(calculate_bubble_points, bubble_rect, new_rect, 0.2)
+	tween.tween_property(self, "bubble_rect", new_rect, 0.4)
+	tween.tween_method(calculate_bubble_points, bubble_rect, new_rect, 0.4)
 #	calculate_bubble_points()
 
 func final_text_format(txt: String) -> String:
@@ -122,11 +131,11 @@ func calculate_bubble_location() -> Vector2:
 
 func calculate_bubble_points(rect: Rect2) -> void:
 	bubble_points_base = offset_bubble_points(rect)
-	prints("bbpb", bubble_points_base)
+#	prints("bbpb", bubble_points_base)
 
 func draw_bubble() -> void:
-#	prints("bubble points", bubble_points)
-	draw_colored_polygon(bubble_points_base, bubble_colour)
+	draw_colored_polygon(bubble_points, bubble_colour)
+	prints("bubble points", bubble_points)
 
 
 func draw_speech_line() -> void:
@@ -136,6 +145,8 @@ func draw_speech_line() -> void:
 func _draw() -> void:
 	if bubble_points.is_empty():
 		return
+#	prints("bubble points", bubble_points)
+	print("drawing!")
 	draw_bubble()
 	draw_speech_line()
 
@@ -153,18 +164,18 @@ func offset_bubble_points(rect: Rect2) -> PackedVector2Array:
 			match i:
 				0:
 					temp_points[i][point] += Vector2(-bubble_padding, -bubble_padding)
-					prints("bipple", i, point, temp_points[i][point])
+#					prints("bipple", i, point, temp_points[i][point])
 				1:
 					temp_points[i][point] += Vector2(bubble_padding + rect.size.x, -bubble_padding)
-					prints("baaple", i, point, temp_points[i][point])
+#					prints("baaple", i, point, temp_points[i][point])
 				2:
 					temp_points[i][point] += Vector2(bubble_padding + rect.size.x, bubble_padding + rect.size.y)
-					prints("boople", i, point, temp_points[i][point])
+#					prints("boople", i, point, temp_points[i][point])
 				3:
 					temp_points[i][point] += Vector2(-bubble_padding, bubble_padding + rect.size.y)
-					prints("beeple", i, point, temp_points[i][point])
+#					prints("beeple", i, point, temp_points[i][point])
 
-	prints("temp points", temp_points)
+#	prints("temp points", temp_points)
 	var tpoints: PackedVector2Array = []
 	for i in 4:
 		tpoints.append_array(temp_points[i])
@@ -172,17 +183,28 @@ func offset_bubble_points(rect: Rect2) -> PackedVector2Array:
 	return tpoints
 
 
-func update_bubble_points(delta: float) -> void:
-	if bubble_points.is_empty():
-		if not bubble_points_base.is_empty():
-			bubble_points = bubble_points_base
-		else:
-			return
+func update_bubble_points() -> void:
+	bubble_points.clear()
 	for point in bubble_points_base.size():
-		bubble_points[point] = Vector2(
-			bubble_points_base[point].x + bubble_point_move_scale * sin(point + delta),
-			bubble_points_base[point].y + bubble_point_move_scale * cos(point + delta)
-			)
+		var newpoint : Vector2
+		var pointoffset : Vector2 = Vector2(
+			bubble_point_move_scale * sin(point + Time.get_ticks_msec()/bubble_oscilate_speed),
+			bubble_point_move_scale * cos(point + Time.get_ticks_msec()/bubble_oscilate_speed)
+		)
+		var pointsign : Vector2 = bubble_points_base[point].sign()
+		prints("pointttt", bubble_points_base[point], pointsign)
+		if pointsign.x < 0:
+			newpoint.x = bubble_points_base[point].x - pointoffset.x
+		else:
+			newpoint.x = bubble_points_base[point].x + pointoffset.x
+		if pointsign.y <  0:
+			newpoint.y = bubble_points_base[point].y - pointoffset.y
+		else:
+			newpoint.y = bubble_points_base[point].y + pointoffset.y
+		bubble_points.append(newpoint)
+#		prints("np", newpoint)
+#	prints("bubble points", bubble_points)
+
 
 
 func update_speech_line() -> void:
